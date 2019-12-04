666821d1fa885e65a002065f24c8bd5f5cdcca60
Krishna Yadav <krishna.yadav@s-force.org>
2019-12-04 22:30:12 +0530

    added threading...

---
+++ com.ascorp.threading/src/com/ascorp/threading/evenodd/PrintOddEvenMain.java
@@ -0,0 +1,15 @@
+package com.ascorp.threading.evenodd;
+public class PrintOddEvenMain {
+	public static void main(String[] args) {
+ 
+		OddEvenRunnable oddRunnable=new OddEvenRunnable(1);
+		OddEvenRunnable evenRunnable=new OddEvenRunnable(0);
+ 
+		Thread t1=new Thread(oddRunnable,"Odd");
+		Thread t2=new Thread(evenRunnable,"Even");
+		
+		t1.start();
+		t2.start();
+ 
+	}
+}
